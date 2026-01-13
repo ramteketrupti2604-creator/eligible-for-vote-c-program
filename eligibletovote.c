08b04f0b15ae469792a0a31789920212d0340141
ramteketrupti2604-creator <ramteketrupti2604@gmail.com>
2026-01-13 11:16:29 +0530

    Add files via upload

---
+++ eligibletovote.c
@@ -0,0 +1,12 @@
+//write a program to check the person eligible for vote or not
+#include<stdio.h>
+int main(){
+    int age=18;
+    if(age>=18){
+        printf("THIS PERSON ELIGIBLE FOR VOTE");
+    }
+    else{
+        printf("THIS PERSON NOT ELIGIBLE FOR VOTE");
+    }
+    return 0;
+}
